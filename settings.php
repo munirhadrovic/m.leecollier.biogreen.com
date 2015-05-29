8e7ecdb8f97b5a9c57856393de48b3461a4c56a9
Novica <novicamiketic@gmail.com>
2015-05-29 11:33:30 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,17 @@
+<?php
+	$adresa[0]="Bio Green of Lee & Collier P.O. Box 34 Estero, FL 33929"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Estero";
+	$telephone[0]=2396720538;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="Bio Green of Lee & Collier 13543 Little Gem Circle Ft. Myers, FL 33913"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]=" Ft. Myers";
+	$telephone[1]=2396720538;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=2;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
